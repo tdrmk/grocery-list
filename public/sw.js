9012792f241873cab00560c278ea0c1a7a25b2c6
Dinesh Ram Kumar Thandapani <tdineshramkumar@gmail.com>
2026-04-09 09:33:12 -0700

    Fix service worker caching error responses (404/500)

--- public/sw.js
+++ public/sw.js
@@ -32,6 +32,7 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
+        if (!response.ok) return response
         const clone = response.clone()
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone))
         return response
